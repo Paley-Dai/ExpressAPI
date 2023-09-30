4d8d0399faf08b6d476613c1526747bd524c0e13
sidghimire <siddharthaghimire@gmail.com>
2023-09-30 23:23:28 +0545

    Upload

--- index.js
+++ index.js
@@ -1,33 +1,11 @@
-const express = require("express");
-const mongoose = require("mongoose");
-
+const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 5173;
+const port = process.env.PORT || 3000;
 
-const URI = "mongodb+srv://webissabin:6DrqLND21bxwlreL@cluster0.d2ioife.mongodb.net/?retryWrites=true&w=majority";
+app.get('/', (req, res) => {
+  res.send('Hello, this is your Express API on Vercel!');
+});
 
-mongoose.connect(URI, mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true }))
-.then(_result => app.listen(PORT))
-.catch(err => console.log(err))
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-
-app.get("/", (_req, res) => {
-    res.send("Async Bananas");
-})
-
-app.post("/data", (req, res) => {
-    try {
-        const numberPlateData = req.body
-    console.log(numberPlateData);
-    } catch (err) {
-        console.log(err);
-        res.status(404).json({error: "Error occurred while processing the request."});
-    }
-    
-})
-
-app.listen(PORT);
-
-module.exports = app;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
