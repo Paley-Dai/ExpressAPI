8b2d7463afddc07150dbb93573cbd87b4beb1623
sabinonweb <sabinonweb7@gmail.com>
2023-10-01 03:25:16 +0545

    initial

--- index.js
+++ index.js
@@ -2,24 +2,37 @@ const mongoose = require('mongoose')
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-app.use(express.json())
-app.use(cors())
-
-mongoose.connect('mongodb+srv://siddharthaghimire:I1Qc3uueefutQfUD@ac-9pvrbjr-shard-00-00.f3ey1v6.mongodb.net:27017,ac-9pvrbjr-shard-00-01.f3ey1v6.mongodb.net:27017,ac-9pvrbjr-shard-00-02.f3ey1v6.mongodb.net:27017/?ssl=true&replicaSet=atlas-xxleoa-shard-0&authSource=admin&retryWrites=true&w=majority',
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }).then(()=>{
-        console.log('Connected to mongodb')
-        app.listen(port, () => {
-            console.log(`Server running on port ${port}`);
-        });
-    })
 
 
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect("mongodb+srv://webissabin:TkgDZMT2CoZPeMwR@cluster0.d2ioife.mongodb.net/?retryWrites=true&w=majority");
+    console.log("MongoDB Connected.");
+    app.listen(port, () => {
+        console.log(`Server running on port ${port}`);
+    });
+  } 
+  catch (error) {
+    console.error(error.message);
+    process.exit(1);
+  }
+}
+
+connectDB()
+
+app.use(express.json()); // This middleware parses JSON request bodies
 
 app.get('/', (req, res) => {
     res.send('Hello, this is your Express API on Vercel!');
 });
 
-
+app.post("/async-bananas", (req, res) => {
+    try {
+        const numberPlate = req.body;
+        console.log(numberPlate)
+        res.send('Received the number plate data successfully');
+    } catch (err) {
+        console.log(err)
+        res.status(500).send('Internal Server Error');
+    }
+});
